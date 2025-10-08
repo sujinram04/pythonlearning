7978ca20fc7de746a5635633efb796dd31dc9d73
sujinram04 <sujinram04@gmail.com>
2025-10-08 22:15:59 +0530

    rock paper scissors game added

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
 print("Hello from VS Code!")
 print("Hi \n" * 3) 
+
+'''
+make a rock paper scissors game
+''' 
+import random
+
+user_choice = input("Enter rock, paper, or scissors: ").lower()
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+
+print(f"Computer chose: {computer_choice}")
+
+
+
+
